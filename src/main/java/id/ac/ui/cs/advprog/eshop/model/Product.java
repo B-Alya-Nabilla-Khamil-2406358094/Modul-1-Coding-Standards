bbfa68c4ce800702e7dfdd5bfbd4eaab2e819c2e
Alyanabilla <alyanabila486@gmail.com>
2026-02-08 10:19:53 +0700

    Create Product Model

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import lombok.getter;
+import lombok.setter;
+
+@getter @setter
+public class Product {
+    private String productId;
+    private String productName;
+    private int productQuantity;
+}
